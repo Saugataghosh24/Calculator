16f0e5be6ef4fecbae418ee134d322b8bf89b06d
Saugata Ghosh <135034312+Saugataghosh24@users.noreply.github.com>
2024-04-02 18:54:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
+let string= "";
+let btns= document.querySelectorAll(".btn");
+let input= document.querySelector("input");
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
+let btnsArray = Array.from(btns);  // creating an array from the btns objects
+for(button of btnsArray){
+    button.addEventListener("click", (e)=>{
+        if(e.target.id == "equal"){
+            string= eval(string);
+            input.value= string;
         }
-
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
+        else if(e.target.innerHTML == "DEL"){
+            string= string.substring(0, string.length-1);
+            input.value= string;
         }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
+        else if(e.target.id == "clear"){
+            string= "";
+            input.value= string;
+        }
+        else if(e.target.innerHTML == "%"){
+            string= string/100;
+            input.value= string;
         }
         else{
-            string += e.target.innerHTML;
-            input.value = string;
+        string= string+ e.target.innerHTML;
+        input.value= string;
         }
-        
-    })
-})
+    });
+}
+
